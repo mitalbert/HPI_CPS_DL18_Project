7a357c554f890248b7c8412fd3d55d4e665b139a
reem94 <reemalrashid94@gmail.com>
2018-07-11 15:39:50 +0200

    Add files via upload
    Adjusting the format of the input and output files according to the submission format.

---
+++ launcher.py
@@ -0,0 +1,41 @@
+import json
+import ntpath
+
+#return from the input file only dict of id & name
+def conv_input(input_data):
+    id_img = dict()
+    for entry in input_data['images']:
+        id_img[entry['id']] = entry['file_name']
+    return id_img
+
+
+def search(name, output):
+    return [element for element in output if getFilename(element['image_name']) == name]
+
+#Get file name from the image_fn which include path
+def getFilename(path):
+    head, tail = ntpath.split(path)
+    return tail or ntpath.basename(head)
+
+#Adjust the output of Captionme with the submission output format
+def covert_output (input_dic , output_dict):
+    data3=[]
+    for key , value in input_dic.items():
+        name = value
+        e = search(name, output_dict)
+        if (len(e) > 0):
+            data3.append({'Image_id' : key , 'Caption' : e[0]['caption']})
+    with open('adj_output.json', 'w') as file:
+         file.write(json.dumps(data3))
+
+
+#assert os.path.isfile("/home/mital/docker/sample-submission/input.json"), "Input JSON missing!"
+
+with open ('output.json') as f1:
+    captionme_dict = json.load(f1)
+with open ('input2.json') as f2:
+    input_data = json.load(f2)
+
+adj_input= conv_input(input_data)
+covert_output(adj_input, captionme_dict)
+
